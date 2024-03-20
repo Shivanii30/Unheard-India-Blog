80ff5502c84c9c3b6ed734177ef9f30b3a9f986f
Shivanii30 <kshirsagarshivani1438@gmail.com>
2024-03-20 22:33:04 +0530

    read blogs dynamically

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-echo "Hey";
+//code for reading and displaying blog posts will be added here
+
+$blogsJson = file_get_contents('Unheard-India-Blog/blogs.json');
+
+// Parse the JSON data into an array
+$blogsArray = json_decode($blogsJson, true);
+
+// Display each blog post using HTML and Bootstrap components
+foreach ($blogsArray as $blog) {
+    echo '<div class="blog-post">';
+    echo '<h2>' . $blog['title'] . '</h2>';
+    echo '<p>Author: ' . $blog['author'] . '</p>';
+    echo '<p>Date: ' . $blog['date'] . '</p>';
+    echo '<p>' . $blog['content'] . '</p>';
+    echo '<img src="' . $blog['image'] . '" alt="Blog Image">';
+    echo '</div>';
+}
 ?>
