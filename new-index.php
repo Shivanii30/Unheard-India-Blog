d08fb51e140431a0a1b83a8511a8ba895502b561
Shivanii30 <kshivaniofficial@gmail.com>
2024-04-03 13:23:07 +0530

    Add Navbar

---
+++ new-index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Unheard India</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Font awesome icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="styles.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="index.js"></script>
+</head>
+
+<body>
+    <header> 
+
+        <nav class = "navbar navbar-expand-lg navbar-light bg-light">
+            <div class = "container">
+                <a href = "new-index.php" class="navbar-brand">Unheard.India</a>
+                <div class="navbar-nav">
+                   <a href="#">home</a>
+                   <a href="submit.php">submit</a>
+                   <a href="about.php">about</a>
+                   <a href="contact.php">connect</a>
+                </div>
+            </div> 
+        </nav>
+
+        <div class = "banner">
+            <div class = "container">
+                <h1 class = "banner-title"><span>Unheard</span> India </h1>
+                <p>Discover the Spiritual Essence of India’s Hidden Sanctuaries</p>
+                <form>
+                <input type="text" class="search-input" placeholder="Begin your Pilgrimage . . .">
+                <button type="submit" class="search-btn"><i class="fas fa-search"></i></button>
+                </form>
+            </div>
+        </div>
+    </header>
