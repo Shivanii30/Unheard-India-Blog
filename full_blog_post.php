187b05ce38cd79a3f76a85ee7aa4efbb275ef39a
Shivanii30 <kshivaniofficial@gmail.com>
2024-04-04 00:46:00 +0530

    Display individual blog pages

--- full_blog_post.php
+++ full_blog_post.php
@@ -20,21 +20,28 @@ if (isset($_GET['id'])) {
     $json_data = file_get_contents('new-blogs.json');
     $blogs = json_decode($json_data, true);
 
-    // Check if the specified blog post ID exists
-    if (isset($blogs[$blog_id])) {
-        $blog = $blogs[$blog_id];
+     // Check if the specified blog post ID exists
+     foreach ($blogs as $blog) {
+        if (isset($blog['id']) && $blog['id'] === $blog_id) {
+            // Display the full content of the blog post
+            echo '<div class="full-blog-post">';
+            echo '<h2>' . $blog['title'] . '</h2>';
+            echo '<p><strong>Author:</strong> ' . $blog['author'] . '</p>';
+            if (!empty($blog['image'])) {
+                echo '<img src="images/' . $blog['image'] . '" alt="' . $blog['title'] . '">';
+            }
+            echo '<p>' . $blog['content'] . '</p>';
+            echo '</div>';
 
-        // Display the full content of the blog post
-        echo '<div class="full-blog-post">';
-        echo '<h2>' . $blog['title'] . '</h2>';
-        echo '<p><strong>Author:</strong> ' . $blog['author'] . '</p>';
-        echo '<img src="images/' . $blog['blog_photo'] . '" alt="' . $blog['title'] . '">';
-        echo '<p>' . $blog['content'] . '</p>';
-        echo '</div>';
+            // Go back button 
+            echo '<a href="index.php" class="go-back-button">Go Back</a>';
 
-        //Go back button 
-        echo '<a href="index.php" class="go-back-button">Go Back</a>';
-    } else {
+            // Exit the loop once the post is found
+            break;
+        }
+    }
+    // If the loop completes without finding the post
+    if (!isset($blog) || (isset($blog) && !isset($blog['id']))) {
         echo '<p>Blog post not found.</p>';
     }
 } else {
